8750e755b23a230d03c2c56daafb9aa8cbdaf943
Grimmy <Lesedi.molokwane@gmail.com>
2023-03-06 15:38:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -pro tip, a program that prints a line with put function
+ * main -a program that prints a line with put function
  * Return: 0 (Success)
  */
 
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
